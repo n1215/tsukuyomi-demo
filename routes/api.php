e60f5d549b62a5f525a298a0fd1fdfe7fef0f898
n1215 <knakae1215@gmail.com>
2017-12-05 10:19:56 +0900

    add a route for a closure handler

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use N1215\Hakudo\RequestMatcher\Path;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /** @var \N1215\Hakudo\Router $router */
 
 $router->add('get', Path::get('|/hello/(?<name>[A-z0-9]*)|'), \App\Http\Handlers\GetHelloHandler::class, [\App\Http\Middleware\HogeMiddleware::class]);
+$router->add('closure', Path::get('|/closure|'), function (ServerRequestInterface $request): ResponseInterface {
+    return new \Zend\Diactoros\Response\JsonResponse(['closure']);
+});
