5f8795ba198dd759435931a041b1c0a1270f624f
Kenji Nakae <nakae@nextat.co.jp>
2017-12-05 23:48:01 +0900

    move EventManager into the framework

--- config/providers.php
+++ config/providers.php
@@ -1,15 +1,14 @@
 <?php
 
 return [
-
     // framework service providers
     \N1215\Tsukuyomi\Providers\BootLoaderServiceProvider::class,
+    \N1215\Tsukuyomi\Providers\EventManagerServiceProvider::class,
+    \N1215\Tsukuyomi\Providers\HttpApplicationServiceProvider::class,
     \N1215\Tsukuyomi\Providers\RequestHandlerFactoryServiceProvider::class,
     \N1215\Tsukuyomi\Providers\RoutingHandlerServiceProvider::class,
-    \N1215\Tsukuyomi\Providers\HttpApplicationServiceProvider::class,
 
     // application service providers
     \App\Providers\RouteServiceProvider::class,
-    \App\Providers\EventManagerServiceProvider::class,
     \App\Providers\HandlerServiceProvider::class,
 ];
