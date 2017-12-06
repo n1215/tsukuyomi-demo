935a604feac280651323eb2415532cc08ad9bb6c
n1215 <knakae1215@gmail.com>
2017-12-06 21:38:48 +0900

    remove deleted service provider

--- config/providers.php
+++ config/providers.php
@@ -5,7 +5,6 @@ return [
     \N1215\Tsukuyomi\Providers\BootLoaderServiceProvider::class,
     \N1215\Tsukuyomi\Providers\HttpApplicationServiceProvider::class,
     \N1215\Tsukuyomi\Providers\RequestHandlerFactoryServiceProvider::class,
-    \N1215\Tsukuyomi\Providers\RoutingHandlerServiceProvider::class,
 
     // application service providers
     \App\Providers\EventServiceProvider::class,
--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 call_user_func(function () {
     $providerClasses = require dirname(__DIR__) . '/config/providers.php';
-    /** @var \Illuminate\Container\Container $container */
     $container = (new \N1215\Tsukuyomi\ContainerBuilder($providerClasses))->build();
     $container->singleton(\N1215\Tsukuyomi\FrameworkInterface::class, function (\Illuminate\Container\Container $container) {
         return new \N1215\Tsukuyomi\Framework($container, realpath(__DIR__ . '/../'));
