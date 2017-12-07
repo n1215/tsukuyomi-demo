a02ca8fd389f2fc26e086848124d67f1526a8b1c
n1215 <knakae1215@gmail.com>
2017-12-07 16:50:39 +0900

    change RoutingErrorResponder signature

--- app/Http/Routing/RoutingErrorResponder.php
+++ app/Http/Routing/RoutingErrorResponder.php
@@ -6,11 +6,12 @@ namespace App\Http\Routing;
 use N1215\Http\Router\RoutingErrorInterface;
 use N1215\Http\Router\RoutingErrorResponderInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 class RoutingErrorResponder implements RoutingErrorResponderInterface
 {
-    public function respond(RoutingErrorInterface $error): ResponseInterface
+    public function respond(ServerRequestInterface $request, RoutingErrorInterface $error): ResponseInterface
     {
         return new JsonResponse(['message' => $error->getMessage()], $error->getStatusCode());
     }
