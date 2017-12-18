c9659a29cc6123d26583d49a66dac7ab106bfe40
n1215 <knakae1215@gmail.com>
2017-12-19 00:07:33 +0900

    adapt router change

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,11 @@ use Zend\Diactoros\Response\JsonResponse;
 
 /** @var \N1215\Hakudo\Router $router */
 
-$router->add('get', Path::get('|/hello/(?<name>[A-z0-9]*)$|'), \App\Handlers\GetHelloHandler::class);
-$router->add('closure', Path::get('|/closure$|'), function (Request $request): Response {
+$router->add(Path::get('|/hello/(?<name>[A-z0-9]*)$|'), \App\Handlers\GetHelloHandler::class)->name('hello');
+$router->add(Path::get('|/closure$|'), function (Request $request): Response {
     return new JsonResponse(['closure']);
-});
+})->name('closure');
 
-$router->add('admin', Path::get('|/admin$|'), function (Request $request): Response {
+$router->add(Path::get('|/admin$|'), function (Request $request): Response {
     return new JsonResponse(['admin area']);
-}, [\App\Middleware\BasicAuth::class]);
+}, [\App\Middleware\BasicAuth::class])->name('admin');
